813c724cc9bb9bbc1b25bcb99dac699b62203de7
Yogen Rai <yogen.rai.992@gmail.com>
2017-05-09 11:58:01 -0500

    Added example for interoperation of DF with RDD

--- spark-examples/course-examples/wikipedia/src/main/scala/sqlExamples/PeopleData.scala
+++ spark-examples/course-examples/wikipedia/src/main/scala/sqlExamples/PeopleData.scala
@@ -4,8 +4,8 @@ import java.io.File
 
 object PeopleData {
 
-  private[sqlExamples] def filePath = {
-    val resource = this.getClass.getClassLoader.getResource("data/people.json")
+  private[sqlExamples] def filePath(fileType: String) = {
+    val resource = this.getClass.getClassLoader.getResource("data/people." + fileType)
     if (resource == null) sys.error("Please put your data file at /src/main/resources path.")
     new File(resource.toURI).getPath
   }
--- spark-examples/course-examples/wikipedia/src/main/scala/sqlExamples/SparkSQLExample.scala
+++ spark-examples/course-examples/wikipedia/src/main/scala/sqlExamples/SparkSQLExample.scala
@@ -1,6 +1,7 @@
 package sqlExamples
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 /**
   * Created by Yogen on 5/8/2017.
   */
@@ -15,8 +16,10 @@ object SparkSQLExample {
                 .master("local")
                 .getOrCreate()
 
-    runBasicDataFrameExample(spark)
-    runDatasetCreationExample(spark)
+//    runBasicDataFrameExample(spark)
+//    runDatasetCreationExample(spark)
+//    runInferSchemaExample(spark)
+    runProgrammaticSchemaExample(spark)
 
     spark.stop()
   }
@@ -25,7 +28,7 @@ object SparkSQLExample {
     //for $ use
     import spark.implicits._
 
-    val df = spark.read.json(PeopleData.filePath)
+    val df = spark.read.json(PeopleData.filePath("json"))
     //display content of dataframe to stdout
     df.show()
 
@@ -54,7 +57,58 @@ object SparkSQLExample {
     val caseClassDS = Seq(Person("Yogen", 32)).toDS()
     caseClassDS.show()
 
-    val peopleDS = spark.read.json(PeopleData.filePath).as[Person]
+    val peopleDS = spark.read.json(PeopleData.filePath("json")).as[Person]
     peopleDS.show()
+  }
+
+  def runInferSchemaExample(spark: SparkSession) = {
+    import spark.implicits._
+
+    val peopleDF = spark.sparkContext.textFile(PeopleData.filePath("txt"))
+                        .map(_.split(","))
+                        .map(attribs => Person(attribs(0), attribs(1).trim.toLong))
+                        .toDF()
+    peopleDF.show()
+    peopleDF.createOrReplaceTempView("people")
+
+    val teenagersDF = spark.sql("SELECT name, age FROM people WHERE age BETWEEN 13 AND 19")
+
+    //access result via field index --> returns name here
+    teenagersDF.map(teenager => teenager(0).toString).show()
+
+    // access result via field name
+    teenagersDF.map(teenager => teenager.getAs[String]("name")).show()
+  }
+
+  def runProgrammaticSchemaExample(spark: SparkSession) = {
+    /**
+      * 1. Create an RDD of Rows from the original RDD;
+      * 2. Create the schema represented by a StructType matching the structure of Rows in the RDD created in Step 1.
+      * 3. Apply the schema to the RDD of Rows via createDataFrame method provided by SparkSession.
+      */
+    import spark.implicits._
+    //create RDD
+    val peopleRDD = spark.sparkContext.textFile(PeopleData.filePath("txt"))
+
+    //convert RDD(people) to Rows
+    val rowRDD = peopleRDD.map(_.split(",")).map(attribs => Row(attribs(0), attribs(1).trim))
+
+    //string encoded schema
+    val schemaString = "name age"
+
+    //generate schema based on string of schema
+    val fields = schemaString.split(" ").map(fieldName => StructField(fieldName, StringType, nullable = true))
+    val schema = StructType(fields)
+
+    //apply schema to the RDD
+    val peopleDF = spark.createDataFrame(rowRDD, schema)
+
+    //crete temp view from DF
+    peopleDF.createOrReplaceTempView("people")
+
+    val results = spark.sql("SELECT name FROM people")
+
+    //results of SQL queries are DF and supports all RDD ops
+    results.map(attributes => attributes(0).toString).show()
   }
 }
