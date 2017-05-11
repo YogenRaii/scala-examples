b9ae2a13234bfe99f3d46456c74bd2731a92d548
Yogen Rai <yogen.rai.992@gmail.com>
2017-05-11 10:56:47 -0500

    Added quicksort example with functional style

---
+++ scala-basics/src/com/eprogrammerz/examples/scala/basics/SortingExample.scala
@@ -0,0 +1,11 @@
+package com.eprogrammerz.examples.scala.basics
+
+class SortingExample {
+  def main(args: Array[String]) : Unit = {
+    
+  }
+  
+  def quickSort(xs: Array[Int]): Array[Int] = {
+    xs
+  }
+}
