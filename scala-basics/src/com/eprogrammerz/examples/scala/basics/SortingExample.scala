5e6fa19762dfb913f7e27ee3bd0226984b634ac5
Yogen Rai <yogen.rai.992@gmail.com>
2017-05-11 11:18:28 -0500

    Added implementation to the QuickSort

--- scala-basics/src/com/eprogrammerz/examples/scala/basics/SortingExample.scala
+++ scala-basics/src/com/eprogrammerz/examples/scala/basics/SortingExample.scala
@@ -1,11 +1,17 @@
 package com.eprogrammerz.examples.scala.basics
 
-class SortingExample {
-  def main(args: Array[String]) : Unit = {
-    
+object SortingExample {
+  def main(args: Array[String]): Unit = {
+    println(quickSort(Array(1, 5, 2, 9, 4, 7)).toList)
   }
-  
+
   def quickSort(xs: Array[Int]): Array[Int] = {
-    xs
+    if (xs.length <= 1) xs
+    else {
+      val pivot = xs(xs.length / 2)
+      Array.concat(quickSort(xs filter (pivot >)),
+        xs filter (pivot ==),
+        quickSort(xs filter (pivot <)))
+    }
   }
 }
