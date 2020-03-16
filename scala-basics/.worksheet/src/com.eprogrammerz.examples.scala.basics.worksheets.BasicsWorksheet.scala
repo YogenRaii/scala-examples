7d7a8d69a0fcc7fd7f35b5d586eed237c4eebca6
Yogen Rai <yogen.rai.992@gmail.com>
2020-03-15 19:25:47 -0500

    Removing unnecessary files

--- scala-basics/.worksheet/src/com.eprogrammerz.examples.scala.basics.worksheets.BasicsWorksheet.scala
+++
@@ -1,9 +0,0 @@
-package com.eprogrammerz.examples.scala.basics.worksheets
-
-object BasicsWorksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(127); 
-  println("Welcome to the Scala worksheet");$skip(33); 
-  
-  var name: String = "Yogen";System.out.println("""name  : String = """ + $show(name ));$skip(34); ;
-  var character: String = "Good";System.out.println("""character  : String = """ + $show(character ));$skip(43); ;
-  println(s"$name is not $character guy");}
-}
