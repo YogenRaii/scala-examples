3c206a86ac639fdf3edcaa49067e692ae462d4d6
Yogen Rai <yogen.rai.992@gmail.com>
2020-03-28 22:10:51 -0500

    refactor for week1 assignments

--- functional-principles/week1/recfun/src/main/scala/common/session.sc
+++ functional-principles/week1/recfun/src/main/scala/common/session.sc
@@ -1,32 +1,33 @@
-var capital = Map("USA" ->"Washigton DC","npela" ->"KTM")
+var capital = Map("USA" -> "Washigton DC", "npela" -> "KTM")
 println(capital)
 
-capital += ("Japan" ->"Tokyo")
+capital += ("Japan" -> "Tokyo")
 
 println(capital)
 
-def factorial(x:BigInt):BigInt = {
-  if (x ==0 ) 1 else x * factorial(x-1)
+def factorial(x: BigInt): BigInt = {
+  if (x == 0) 1 else x * factorial(x - 1)
 }
 
 factorial(30)
 
 import scala.collection.mutable.Map
-val treasureMap = Map[Int,String]()
+
+val treasureMap = Map[Int, String]()
 treasureMap += (1 -> "Go to island.")
-treasureMap += (2 ->"Yogen")
-treasureMap += (3 ->"Dig.")
+treasureMap += (2 -> "Yogen")
+treasureMap += (3 -> "Dig.")
 
 println(treasureMap(2))
 
-def printArgs(args :Array[String]):Unit = {
+def printArgs(args: Array[String]): Unit = {
   args.foreach(println)
-//  for(arg <- args){
-//    print("jpt")
-//    println(arg)
-//  }
+  //  for(arg <- args){
+  //    print("jpt")
+  //    println(arg)
+  //  }
 }
 
-val names = Array("Yogen","Rai")
+val names = Array("Yogen", "Rai")
 printArgs(names)
 println()
--- functional-principles/week1/recfun/src/main/scala/recfun/Main.scala
+++ functional-principles/week1/recfun/src/main/scala/recfun/Main.scala
@@ -14,35 +14,37 @@ object Main {
     * Exercise 1
     */
   def pascal(c: Int, r: Int): Int = {
-    if(r == c || c<=0) 1
-    else pascal(c,r-1)+pascal(c-1,r-1)
+    if (r == c || c == 0) 1
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
   }
 
   /**
     * Exercise 2
     */
   def balance(chars: List[Char]): Boolean = {
-    def balanced(chars: List[Char], count:Int):Boolean= chars match {
-      case Nil=> count == 0
-      case _ if chars.head == '(' => balanced(chars.tail,count+1)
-      case _ if chars.head == ')' =>count > 0 && balanced(chars.tail,count-1)
-      case _ => balanced(chars.tail,count)
+    def balanced(chars: List[Char], count: Int): Boolean = chars match {
+      case Nil => count == 0
+      case _ if chars.head == '(' => balanced(chars.tail, count + 1)
+      case _ if chars.head == ')' => count > 0 && balanced(chars.tail, count - 1)
+      case _ => balanced(chars.tail, count)
     }
-    balanced(chars,0)
+
+    balanced(chars, 0)
   }
 
   /**
     * Exercise 3
     */
   def countChange(money: Int, coins: List[Int]): Int = {
-    def recurs(m:Int, cs: List[Int], count:Int):Int = {
-      if (m<0) count
-      else if(cs.isEmpty){
-        if (m ==0 ) count+1
+    def recurs(m: Int, cs: List[Int], count: Int): Int = {
+      if (m < 0) count
+      else if (cs.isEmpty) {
+        if (m == 0) count + 1
         else count
       }
-      else recurs(m,cs.tail,count) + recurs(m-cs.head,cs,count)
+      else recurs(m, cs.tail, count) + recurs(m - cs.head, cs, count)
     }
-    recurs(money,coins,0)
+
+    recurs(money, coins, 0)
   }
 }
